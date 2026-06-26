6fc9bc1ea3fbb9f60401eef0a6e6127b1c647f5c
qoqos <qq@gm.com>
2026-06-26 16:35:35 +0500

    update

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,16 +4,19 @@ import CustomBtn from "@components/UI/CustomBtn";
 import type { FC } from "react";
 import { Link } from "react-router";
 import { useForm } from "react-hook-form";
-import type { ILogin } from "@/types";
+import type { IRegister } from "@/types";
 
 const Register: FC = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
-  } = useForm<ILogin>({ mode: "onChange" });
+  } = useForm<IRegister>({ mode: "onChange" });
 
-  const submit = (data: ILogin) => {
+  const password = watch("password");
+
+  const submit = (data: IRegister) => {
     console.log("data", data);
     console.log(errors);
   };
@@ -22,22 +25,63 @@ const Register: FC = () => {
       <LayoutForm>
         <div className="layoutForm__right-block">
           <h1 className="layoutForm__right-block_title">Регистрация</h1>
-          <form className="layoutForm__right-block_form">
-            <CustomInput placeholder="Имя" type="text" text="Ваше Имя" />
+          <form
+            className="layoutForm__right-block_form"
+            onSubmit={handleSubmit(submit)}
+          >
+            <CustomInput
+              placeholder="Имя"
+              type="text"
+              text="Ваше Имя"
+              error={errors.username}
+              register={register("username", {
+                required: "Это поле обязательно для заполнения",
+                minLength: {
+                  value: 6,
+                  message: "Минимальная длина 6 символа",
+                },
+              })}
+            />
             <CustomInput
               placeholder="Ваша почта"
-              type="password"
+              type="email"
               text="Почта"
+              error={errors.username}
+              register={register("email", {
+                required: "Это поле обязательно для заполнения",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Некорректный email",
+                },
+              })}
             />
             <CustomInput
               placeholder="Ваш пароль"
               type="password"
               text="Пароль"
+              error={errors.password}
+              register={register("password", {
+                required: "Это поле обязательно для заполнения",
+                minLength: {
+                  value: 8,
+                  message: "Минимальная длина 8 символа",
+                },
+              })}
             />
             <CustomInput
               placeholder="Повторите пароль"
               type="password"
               text="Повторите пароль"
+              error={errors.password2}
+              register={register("password2", {
+                required: "Это поле обязательно для заполнения",
+                validate: (value) =>
+                  value === password || "Пароли не совпадают",
+                minLength: {
+                  value: 8,
+                  message: "Минимальная длина 8 символов",
+                },
+              })}
             />
           </form>
           <CustomBtn
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,10 @@ export interface ILogin {
   username: string;
   password: string;
 }
+
+export interface IRegister {
+    username: string;
+    password: string;
+    password2: string;
+    email: string;
+}
